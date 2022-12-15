8581f2891e7898f8aaea8321cb53d9cc6f82c792
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-12-15 16:18:32 +0700

    Add create Queues and declare variable

--- CircularQueues_B/Program.cs
+++ CircularQueues_B/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace CircularQueues_B
 {
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
