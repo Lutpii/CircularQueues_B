14eccebbe597bda3c60e1ae7686af5a00f83d38a
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-12-15 16:19:54 +0700

    add method insert

--- CircularQueues_B/Program.cs
+++ CircularQueues_B/Program.cs
@@ -15,9 +15,28 @@ namespace CircularQueues_B
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int element)
+        {
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overvlow\n");
+                return;
+            }
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = element;
+        }
     }
-
-
     internal class Program
     {
         static void Main(string[] args)
